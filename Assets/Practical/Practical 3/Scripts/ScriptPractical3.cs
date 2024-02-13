227b9b5a0767edb06d7a863ba37f47b76e8ab0db
Туан <8268826@gmail.com>
2024-02-13 21:37:35 +0300

    Add practical 3 4

---
+++ Assets/Practical/Practical 3/Scripts/ScriptPractical3.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ScriptPractical3 : MonoBehaviour
+{
+    public int randomNumber;
+    public GameObject someObject;
+
+    private void Update()
+    {
+        Randomeizer();
+    }
+
+    void Randomeizer()
+    {
+        randomNumber = Random.Range(1, 100); //[1;99)
+        Debug.Log(randomNumber);
+
+        if (randomNumber == 3)
+        {
+            Debug.Log("Проверка условия!");
+        }
+    }
+}
---
+++ Assets/Practical/Practical 4/Scripts/ScriptPractical4.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ScriptPractical4 : MonoBehaviour
+{
+    private Vector3 originalPos;
+
+    private void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        transform.position = originalPos;
+    }
+}

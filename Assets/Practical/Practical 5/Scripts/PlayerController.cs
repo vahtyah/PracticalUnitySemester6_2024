beb2edd2e88b09ad6eccbd8920fe2a8cdce3c830
Туан <8268826@gmail.com>
2024-02-13 22:37:29 +0300

    update practical 5

--- Assets/Practical/Practical 5/Scripts/PlayerController.cs
+++ Assets/Practical/Practical 5/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpForce = 7.0f;
+    public LayerMask groundMask;
+    public Transform groundCheck;
     private float movementX;
     private float movementZ;
     private Rigidbody rb;
-    [SerializeField] private bool isGrounded;
+    private bool isGrounded;
 
     void Start()
     {
@@ -16,40 +18,20 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Обработка ввода для движения
         movementX = Input.GetAxis("Horizontal") * speed;
         movementZ = Input.GetAxis("Vertical") * speed;
 
-        // Проверка нажатия клавиши прыжка и условия нахождения на земле
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            Debug.Log("CCgi");
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, .2f, groundMask);
     }
 
     void FixedUpdate()
     {
-        // Применение движения
         Vector3 movement = transform.right * movementX + transform.forward * movementZ;
         rb.MovePosition(transform.position + movement * Time.fixedDeltaTime);
-    }
-
-    void OnCollisionEnter(Collision other)
-    {
-        // Проверка столкновения с землей
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-
-    void OnCollisionExit(Collision other)
-    {
-        // Проверка выхода из столкновения с землей
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
     }
 }
